8c28841deee8e9581a2bfb6d90a69cf2e58a6f04
Gustavo Armagno <gustavoa@gmail.com>
2023-11-27 14:54:30 -0300

    Wrap backend exceptions for i2c

--- supernovacontroller/sequential/i2c.py
+++ supernovacontroller/sequential/i2c.py
@@ -1,5 +1,6 @@
 from transfer_controller import TransferController
 from BinhoSupernova.Supernova import Supernova
+from supernovacontroller.errors import BackendError
 
 
 class SupernovaI2CBlockingInterface:
@@ -9,37 +10,52 @@ class SupernovaI2CBlockingInterface:
 
     def set_parameters(self, voltage: float, rate: int = 1000000):
         voltage_int = voltage * 1000
-        result = self.controller.sync_submit([
-            lambda transfer_id: self.driver.setI2cSpiUartBusVoltage(transfer_id, voltage_int),
-            lambda transfer_id: self.driver.i2cSetParameters(transfer_id, baudrate=rate),
-        ])
+        try:
+            result = self.controller.sync_submit([
+                lambda transfer_id: self.driver.setI2cSpiUartBusVoltage(transfer_id, voltage_int),
+                lambda transfer_id: self.driver.i2cSetParameters(transfer_id, baudrate=rate),
+            ])
+        except Exception as e:
+            raise BackendError(original_exception=e) from e
 
         return result
 
     def write(self, address, register, data):
-        result = self.controller.sync_submit([
-            lambda transfer_id: self.driver.i2cWrite(transfer_id, address, register, data),
-        ])
+        try:
+            result = self.controller.sync_submit([
+                lambda transfer_id: self.driver.i2cWrite(transfer_id, address, register, data),
+            ])
+        except Exception as e:
+            raise BackendError(original_exception=e) from e
 
         return result
 
     def write_non_stop(self, address, register, data):
-        responses = self.controller.sync_submit([
-            lambda transfer_id: self.driver.i2cWriteNonStop(transfer_id, address, register, data),
-        ])
+        try:
+            responses = self.controller.sync_submit([
+                lambda transfer_id: self.driver.i2cWriteNonStop(transfer_id, address, register, data),
+            ])
+        except Exception as e:
+            raise BackendError(original_exception=e) from e
 
         return responses
 
     def read(self, address, length):
-        responses = self.controller.sync_submit([
-            lambda transfer_id: self.driver.i2cRead(transfer_id, address, length),
-        ])
+        try:
+            responses = self.controller.sync_submit([
+                lambda transfer_id: self.driver.i2cRead(transfer_id, address, length),
+            ])
+        except Exception as e:
+            raise BackendError(original_exception=e) from e
 
         return responses
 
     def read_from(self, address, register, length):
-        responses = self.controller.sync_submit([
-            lambda transfer_id: self.driver.i2cReadFrom(transfer_id, address, register, length),
-        ])
+        try:
+            responses = self.controller.sync_submit([
+                lambda transfer_id: self.driver.i2cReadFrom(transfer_id, address, register, length),
+            ])
+        except Exception as e:
+            raise BackendError(original_exception=e) from e
 
         return responses
