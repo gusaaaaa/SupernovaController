98bc9d8b24f8fc7e7777ddabb674c59dbe404d81
unknown <jpons@focus.uy>
2023-11-30 16:01:33 -0300

    Add description to the example

--- examples/basic_i3c_example.py
+++ examples/basic_i3c_example.py
@@ -8,6 +8,20 @@ def find_matching_item(data, target_pid):
     return None
 
 def main():
+    """
+    Basic example to illustrate i3c protocol usage with SupernovaController.
+    
+    Sequence of commands:
+    - Initialize the Device: Creates and opens a connection to Supernova host adapter.
+    - Create I3C Interface: Creates an I3C interface for communication.
+    - Set I3C Parameters and Initialize Bus: Sets transfer rates and initializes the I3C bus with a specific voltage level.
+    - Discover Targets on I3C Bus: Fetches a list of devices present on the I3C bus.
+    - Find Specific ICM Device: Uses find_matching_item to find a specific device based on its PID.
+    - Perform CCC (Common Command Code) Transfers: Sends various CCC commands to the target device to get or set parameters.
+    - Write/Read Transfers: Demonstrates write and read operations over the I3C bus.
+    - Reset I3C Bus and Targets: Resets the bus and fetches the target list again.
+    - Close Device Connection: Closes the connection to the Supernova device.
+    """
     device = SupernovaDevice()
 
     info = device.open()
@@ -41,36 +55,45 @@ def main():
     # CCC Transfers
     # ---
     result = i3c.ccc_getpid(target_address)
-    print(result)
+    print(result[1])
     result = i3c.ccc_getbcr(target_address)
-    print(result)
+    print(result[1])
     result = i3c.ccc_getdcr(target_address)
-    print(result)
+    print(result[1])
     result = i3c.ccc_getcaps(target_address)
-    print(result)
+    print(result[1])
     result = i3c.ccc_getmxds(target_address)
-    print(result)
+    print(result[1])
     result = i3c.ccc_getmrl(target_address)
-    print(result)
+    print(result[1])
     result = i3c.ccc_unicast_setmrl(target_address, 1024)
-    print(result)
+    print(result[1])
     result = i3c.ccc_broadcast_setmrl(64)
-    print(result)
+    print(result[1])
     result = i3c.ccc_getmwl(target_address)
-    print(result)
+    print(result[1])
     result = i3c.ccc_unicast_setmwl(target_address, 80)
-    print(result)
+    print(result[1])
     result = i3c.ccc_broadcast_setmwl(256)
-    print(result)
+    print(result[1])
 
     # ---
     # Write/Read transfers
     # ---
     result = i3c.write(target_address, i3c.TransferMode.I3C_SDR, [0x16], [0x40])
+    print(result[1])
     result = i3c.read(target_address, i3c.TransferMode.I3C_SDR, [0x16], 2)
+    print(result[1])
+
+    # ---
+    # Target reset
+    # ---
+    result = i3c.reset_bus()
+    print(result[1])
+    result = i3c.targets()
+    print(result[1])
 
     device.close()
-
 
 if __name__ == "__main__":
     main()
