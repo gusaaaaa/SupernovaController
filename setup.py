41ab091dd9445b15831b2b220343ba09c15883ee
Gustavo Armagno <gustavoa@gmail.com>
2023-12-01 15:39:06 -0300

    Update setup and readme

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(
     name='supernovacontroller',
     version='0.1.0',
     packages=find_packages(),
+    data_files=[
+        ('SupernovaExamples', ['examples/basic_i2c_example.py', 'examples/basic_i3c_example.py', 'examples/ibi_example.py', 'examples/ICM42605_i3c_example.py'])
+    ],
     description='A blocking API for interacting with the Supernova host-adapter device',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
