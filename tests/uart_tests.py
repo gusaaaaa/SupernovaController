bc31675fd287e2f3fe09d6714ee82effc16be1c6
Rafael Alonso <santiago.ralonso@correo.ucu.edu.uy>
2024-08-06 16:30:30 -0300

    1380 | Apply suggestions

--- tests/uart_tests.py
+++ tests/uart_tests.py
@@ -1,10 +1,16 @@
-import threading
-import unittest, sys, os
-from supernovacontroller.sequential import SupernovaDevice
-from BinhoSupernova.commands.definitions import UartControllerBaudRate as baud, UartControllerParity as parity, UartControllerDataSize as dataSize, UartControllerStopBit as stopBit
+import os  
+import sys  
+import threading  
+import unittest  
 
-from supernovacontroller.sequential.uart import SupernovaUARTBlockingInterface
-
+from supernovacontroller.sequential import SupernovaDevice  
+from supernovacontroller.sequential.uart import SupernovaUARTBlockingInterface  
+from BinhoSupernova.commands.definitions import (  
+    UartControllerBaudRate as baud,  
+    UartControllerParity as parity,  
+    UartControllerDataSize as dataSize,  
+    UartControllerStopBit as stopBit  
+)
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from binhosimulators import BinhoSupernovaSimulator
 
@@ -16,12 +22,16 @@ class TestSupernovaControllerUART(unittest.TestCase):
         Initializes the testing class. Determines whether to use the simulator or real device
         based on the "USE_REAL_DEVICE" environment variable. Default is to use the simulator.
         """
+        # NOTE: These tests only work for the real device until BMC2-1512 is solved
         cls.use_simulator = False
 
     def setUp(self):
         self.device = SupernovaDevice()
         if self.use_simulator:
             self.device.driver = BinhoSupernovaSimulator()
+
+    def tearDown(self):
+        self.device.close()
 
     def test_uart_echo_message_real_device(self):
         if self.use_simulator:
@@ -41,8 +51,6 @@ class TestSupernovaControllerUART(unittest.TestCase):
         self.assertEqual(success, True)
         self.assertTupleEqual((successReceive, response), (True, data), f"Failed echo: {response}")
 
-        self.device.close()
-
     def test_set_bus_voltage(self):
         self.device.open()
         uart : SupernovaUARTBlockingInterface = self.device.create_interface("uart") # Type hinting for easier development
@@ -51,8 +59,6 @@ class TestSupernovaControllerUART(unittest.TestCase):
 
         self.assertEqual(success, True)
 
-        self.device.close()
-
     def test_wait_timeout(self):
         self.device.open()
         uart : SupernovaUARTBlockingInterface = self.device.create_interface("uart") # Type hinting for easier development
@@ -60,26 +66,18 @@ class TestSupernovaControllerUART(unittest.TestCase):
         uart.set_bus_voltage(3300)
         uart.init_bus()
 
-        threading.Timer(5, lambda: self.fail("Timeout was exceeded"))
-        (success, response) = uart.wait_for_notification(4)
-        self.assertTupleEqual((False, "Timeout occurred while waiting for the UART receive notification"), (success, response), f"Failed non-timeout response: {response}")
+        timer = threading.Timer(5, lambda: self.fail("Timeout was exceeded"))  
+        timer.start() 
 
-        self.device.close()
-
-    def test_uart_init_bus(self):
-        if self.use_simulator:
-            self.skipTest("For real device only")
-            # Currently there is a discrepancy in signature for the init bus methods between simulator and real SDK, making this test fail with the SIM
-
-        self.device.open()
-        uart : SupernovaUARTBlockingInterface = self.device.create_interface("uart") # Type hinting for easier development
-
-        (success, response) = uart.set_bus_voltage(330000)
-        self.assertEqual(False, success, f"Invalid excessive voltage set was successful: {response}")
-        (success, response) = uart.set_bus_voltage(3)
-        self.assertEqual(False, success, f"Invalid low voltage set was successful: {response}")
-
-        self.device.close()
+        try:  
+            success, response = uart.wait_for_notification(4)  
+            self.assertTupleEqual(  
+                (False, "Timeout occurred while waiting for the UART receive notification"),  
+                (success, response),  
+                f"Failed non-timeout response: {response}"  
+            )  
+        finally:  
+            timer.cancel()
 
     # TODO Fails due to lack of management of ALREADY_INITIALIZED in the blocking interface
     def test_uart_init_bus(self):
@@ -94,8 +92,6 @@ class TestSupernovaControllerUART(unittest.TestCase):
         (success, response) = uart.init_bus()
 
         self.assertEqual(success, True, f"Failed: {response}")
-
-        self.device.close()
 
     # TODO Fails due to lack of management of ALREADY_INITIALIZED in the blocking interface
     def test_uart_multiple_init_bus(self):
@@ -113,8 +109,6 @@ class TestSupernovaControllerUART(unittest.TestCase):
 
         (success, response) = uart.init_bus()
         self.assertTupleEqual((False, "UART_ALREADY_INITIALIZED_ERROR"), (success, response), f"Failed on second init: {response}") # Revise this once the error is better handled, it is not necessarily the way forward presenting the error
-
-        self.device.close()
 
     def test_uart_set_get_params(self):
         if self.use_simulator:
@@ -141,9 +135,6 @@ class TestSupernovaControllerUART(unittest.TestCase):
         parsed_response = [response[index].name if not isinstance(response[index], bool) else response[index] for index in range(len(response))]
 
         self.assertTupleEqual((True, param_names), (success, parsed_response))
-
-        self.device.close()
-
 
 if __name__ == "__main__":
     unittest.main()
