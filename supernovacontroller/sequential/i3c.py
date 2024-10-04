2390d5437c702c5dd4bf308ab1f357ecb14db53b
Rafael Alonso <santiago.ralonso@correo.ucu.edu.uy>
2024-10-04 14:59:59 -0300

    1797 | Fix init bus error checking

--- supernovacontroller/sequential/i3c.py
+++ supernovacontroller/sequential/i3c.py
@@ -186,7 +186,7 @@ class SupernovaI3CBlockingInterface:
         # TODO: Toggle IBIs off
 
         status = responses[0]["result"]
-        if status == "DAA_SUCCESS" and responses[0]["errors"] == "NO_TRANSFER_ERROR":
+        if status == "DAA_SUCCESS" and "NO_TRANSFER_ERROR" in responses[0]["errors"]:
             result = (True, voltage)
         else:
             result = (False, {"errors": responses[0]["result"]})
